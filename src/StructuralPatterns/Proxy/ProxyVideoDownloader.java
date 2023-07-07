ec4d2d4bfa68d8700e3514900272e11267981824
NadavMozeson <nadavson02@gmail.com>
2023-07-07 22:18:48 +0300

    Improved Proxy

--- src/StructuralPatterns/Proxy/ProxyVideoDownloader.java
+++ src/StructuralPatterns/Proxy/ProxyVideoDownloader.java
@@ -5,12 +5,12 @@ import java.util.Map;
 
 public class ProxyVideoDownloader implements VideoDownloader{
     private final Map<String, Video> videoCache = new HashMap<>();
-    private final VideoDownloader downloader = new RealVideoDownloader();
+    private final VideoDownloader realSubject = new RealVideoDownloader();
 
     @Override
     public Video getVideo(String videoName) {
         if (!videoCache.containsKey(videoName)){
-            videoCache.put(videoName, downloader.getVideo(videoName));
+            videoCache.put(videoName, realSubject.getVideo(videoName));
         }
         return videoCache.get(videoName);
     }
