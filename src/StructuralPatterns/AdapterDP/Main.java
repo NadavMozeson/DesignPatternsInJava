7872ade95baaab84d16f289e08fc5b57bf2bf457
NadavMozeson <nadavson02@gmail.com>
2023-07-07 14:25:06 +0300

    Renamed files to Main

--- src/StructuralPatterns/AdapterDP/Main.java
+++ src/StructuralPatterns/AdapterDP/Main.java
@@ -1,6 +1,6 @@
 package StructuralPatterns.AdapterDP;
 
-public class AdapterDemo {
+public class Main {
     public static void main(String[] args) {
         RectangularPlug plug = new RectangularPlug("rectStem1", "rectStem2");
         plug.getPower(); // -> "Power power power..."
