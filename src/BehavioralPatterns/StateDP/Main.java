da902a11292891e67ce27fc76516f1c4ffee7be7
NadavMozeson <nadavson02@gmail.com>
2023-07-09 15:14:07 +0300

    Fixed State

--- src/BehavioralPatterns/StateDP/Main.java
+++ src/BehavioralPatterns/StateDP/Main.java
@@ -3,11 +3,8 @@ package BehavioralPatterns.StateDP;
 public class Main {
     public static void main(String[] args){
         Phone stateContext = new Phone();
-        stateContext.alert();
-        stateContext.alert();
+        stateContext.alert(); // -> "vibration..."
         stateContext.setState(new Silent());
-        stateContext.alert();
-        stateContext.alert();
-        stateContext.alert();
+        stateContext.alert(); // -> "silent..."
     }
 }
